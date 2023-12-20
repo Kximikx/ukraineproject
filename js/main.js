1d9daf68daa5e352e88192d3b758b15b8d0e7463
kximikx <mushiiyura@gmail.com>
2023-12-20 20:43:10 +0200

    Спроби виправлення певних багів та спроба підключення лічильника

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ $(document).ready(function(){
     appendArrows:('.slider_buttons')
     });
 });
+//Лічильник
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value");
+animateValue(obj, 1000, 80, 9, 27);
