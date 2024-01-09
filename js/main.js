6a6aebf13f041398067944ba995cf4123852ccbd
kximikx <mushiiyura@gmail.com>
2024-01-09 18:06:59 +0200

    Фікс багів

--- js/main.js
+++ js/main.js
@@ -17,22 +17,12 @@ $(document).ready(function(){
       autoplaySpeed: 3000,
       arrows:false
       });
+      //Counter
+      $('#counter').counter('start')({
+        duration: 5000,
+        countFrom:0, 
+        counTo:1000
+      });
 });
 
 
-//Лічильник
-function animateValue(obj, start, end, duration) {
-  let startTimestamp = null;
-  const step = (timestamp) => {
-    if (!startTimestamp) startTimestamp = timestamp;
-    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-    obj.innerHTML = Math.floor(progress * (end - start) + start);
-    if (progress < 1) {
-      window.requestAnimationFrame(step);
-    }
-  };
-  window.requestAnimationFrame(step);
-}
-
-const obj = document.getElementById("value");
-animateValue(obj, 1000, 80, 9, 27);
